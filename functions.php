95c7440f516de6dccaf27f11f754666261e05a7a
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-08 23:38:59 +0100

    test add logo

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+	 * Site branding wrapper and display
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	function storefront_site_branding() {
+		?>
+		<div class="site-branding">
+			<?php storefront_site_title_or_logo(); ?>
+		</div>
+		<?php
+    }
+    
